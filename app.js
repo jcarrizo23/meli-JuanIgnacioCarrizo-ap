a8a139292e756f2d3d61488f6f034b55cf331b7f
jcarrizo23 <carrizojuanignacio23@gmail.com>
2023-11-13 15:46:48 -0300

    actualizacion para render

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-   console.log('Servidor corriendo en el puerto 3000');
-});
+const port = process.env.PORT || 3001;
+app.listen (port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
-})
+});
+
